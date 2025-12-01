ffec485c6cfc1124d4ba0a8cabfb5ca4de96b9a2
Manuel Kyd Thomas Nagpala <kydnagpala.dev@gmail.com>
2025-12-01 23:23:15 +0800

    chore: update ui component file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,20 @@
 'use client';
 
 import Hero from "@/components/layouts//hero";
-import ParticleBackground from "@/components/layouts//ParticleBackground";
-import CustomCursor from "@/components/layouts//CustomCursor";
-import ScrollButton from "@/components/layouts//ScrollButton";
-import Image from "next/image";
+import ParticleBackground from "@/components/ui/ParticleBackground";
+import CustomCursor from "@/components/ui/CustomCursor";
+import ScrollButton from "@/components/ui/ScrollButton";
 import { useState, useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Navbar from "@/components/layouts//Navbar";
 import AboutMe from "@/components/layouts//AboutMe";
-import { experienceData } from "@/data/Experience.data";
-import { projectData } from "@/data/Project";
 import Project from "@/components/layouts/Project";
 import Contact from "@/components/layouts/Contact";
 import Experience from "@/components/layouts/Experience";
 import Footer from "@/components/layouts/Footer";
 
 export default function Home() {
-  const [activeSection, setActiveSection] = useState('home');
-
   useEffect(() => {
     AOS.init({
       duration: 800,
@@ -30,13 +25,6 @@ export default function Home() {
       delay: 0,
     });
   }, []);
-
-  const scrollToSection = (sectionId: string) => {
-    const element = document.getElementById(sectionId);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
 
   return (
     <div className="relative min-h-screen bg-[#0a0e27] text-white overflow-x-hidden cursor-none">
@@ -63,7 +51,7 @@ export default function Home() {
       {/* Contact Section */}
 
       <Contact />
-      
+
       {/* Footer Section */}
       <Footer />
 
